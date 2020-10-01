02747e53871cae82176c632023a79796ee015b70
noelvandevrie <nvdvrie@gmail.com>
2020-10-02 00:31:54 +0200

    fixed stylesheet working and removed test code

---
+++ app.js
@@ -0,0 +1,18 @@
+(function () {
+  // Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9CpENXN25PjdqqwYwlaLshKGst2o3gcg",
+    authDomain: "juvo-app.firebaseapp.com",
+    databaseURL: "https://juvo-app.firebaseio.com",
+    projectId: "juvo-app",
+    storageBucket: "juvo-app.appspot.com",
+    messagingSenderId: "293449225332",
+    appId: "1:293449225332:web:99d40aab2780e327b66d21",
+    measurementId: "G-H1KKBECR0V",
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  //Get Elements
+  const txtEmail = document.getElementById("txtEmail");
+  const txtPassword = document.getElementById("");
+})();
